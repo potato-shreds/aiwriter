9412598c018218d53e20a5476c7cc2745ae026a0
aigcdev <gptaigc@gmail.com>
2024-11-19 11:26:07 +0800

    luyoutiaozhuan

--- data/config.ts
+++ data/config.ts
@@ -1,7 +1,7 @@
 const config = {
   // url: `ai-writer.app`,
   url: `34.168.109.32:8095`,
-  httpUrl: `http://34.145.121.90:8040/`,
+  httpUrl: `http://34.168.109.32:8095/`,
 };
 
 export default config;
