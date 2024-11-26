5e94cf89284e1773dca1c4e2e131337b598d7d84
aigcdev <gptaigc@gmail.com>
2024-11-26 10:40:50 +0800

    fix:template url 404

--- data/config.ts
+++ data/config.ts
@@ -1,6 +1,6 @@
 const config = {
   url: `ai-writer.app`,
-  httpUrl: `ai-writer.app/`,
+  httpUrl: `https://ai-writer.app/`,
   // url: `34.168.109.32:8095`,
   // httpUrl: `http://34.168.109.32:8095/`,
   serverURL: 'http://34.168.109.32:8030',
